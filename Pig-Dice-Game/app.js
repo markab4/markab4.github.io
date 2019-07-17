47ed8e81c219c858aa2ebc3518e26b44d99378a7
Mark Abramov <markabramov01@gmail.com>
2019-07-17 10:44:15 -0400

    game play functionality

--- Pig-Dice-Game/app.js
+++ Pig-Dice-Game/app.js
@@ -4,12 +4,12 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
-let scores = [0,0],
+let scores = [0, 0],
     roundScore = 0,
     activePlayer = 0;
 let diceDom = document.querySelector(".dice");
@@ -21,27 +21,47 @@ document.getElementById('current-1').textContent = '0';
 
 diceDom.style.display = "none";
 
-document.querySelector(".btn-roll").addEventListener("click", function(){
+function nextPlayer() {
+    activePlayer = (activePlayer === 0 ? 1 : 0);
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    diceDom.style.display = 'none';
+}
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
     // random number
     let dice = Math.floor(Math.random() * 6) + 1;
     // display result
     diceDom.style.display = "block";
     diceDom.src = "dice-" + dice + ".png";
     //update round score if number not a 1
-    if(dice !== 1){
+    if (dice !== 1) {
         roundScore += dice;
         document.querySelector("#current-" + activePlayer).textContent = roundScore;
     } else { // if player scores a 1
-        activePlayer = (activePlayer === 0 ? 1 : 0);
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        diceDom.style.display = 'none';
+        nextPlayer();
     }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+    // update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //check if player won the game
+    if(scores[activePlayer] >= 20 ) {
+        document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+        diceDom.style.display = "none";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    }
+    else
+        nextPlayer();
 });
 
